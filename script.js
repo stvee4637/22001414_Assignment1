b05b5ec42297c5030c7bfc8e84343c1952360f8e
Steven <steven@weblaunch.co.nz>
2026-09-13 13:08:49 +1200

    wip: navbar and landing screen css

---
+++ script.js
@@ -0,0 +1,9 @@
+// Student ID: 22001414
+window.addEventListener("scroll", function () {
+  const navbar = document.getElementById("navbar");
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
