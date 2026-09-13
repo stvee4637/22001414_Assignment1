c95f49b1f7c2b23d77960882d4ba2fd08050f9d4
Steven <steven@weblaunch.co.nz>
2026-09-13 23:40:08 +1200

    chore: added in assignment copywriting, and cleaned up styling
    feat: added popup

--- script.js
+++ script.js
@@ -2,8 +2,22 @@
 window.addEventListener("scroll", function () {
   const navbar = document.getElementById("navbar");
   if (window.scrollY > 50) {
-    navbar.classList.add("scrolled");
+    navbar.classList.add("scroll");
   } else {
-    navbar.classList.remove("scrolled");
+    navbar.classList.remove("scroll");
   }
+});
+
+// window.addEventListener("load", function () {
+//   setTimeout(function open(event) {
+//     document.querySelector(".popup").style.display = "flex";
+//   }, 1000);
+// });
+
+document.querySelector("#close").addEventListener("click", function () {
+  document.querySelector(".popup").style.display = "none";
+});
+
+document.querySelector("#popup-cta").addEventListener("click", function () {
+  document.querySelector(".popup").style.display = "none";
 });
